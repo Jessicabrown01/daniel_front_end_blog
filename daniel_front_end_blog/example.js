ea8a6be83b4b49b2691af2be667561aec2f5eb18
jessica brown <jjessicabrown11@gmail.com>
2022-09-06 14:21:05 +0100

    first commit

---
+++ daniel_front_end_blog/example.js
@@ -0,0 +1,7 @@
+const age = [1,2,3,4,5]
+const  functionExample = function () {
+    console.log("we are learning react")
+}
+let name = "Daniel";
+export default functionExample;
+export {age, name}
